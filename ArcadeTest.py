b3ea3ea95cdbe358611b48d94b14ca3eec2694eb
LouisEmery <emerylo@educ.college-montreal.qc.ca>
2022-10-26 10:46:29 -0400

    aaa

--- ArcadeTest.py
+++ ArcadeTest.py
@@ -1,15 +1,23 @@
 import arcade
-
+import os
+path = r'C:\Utilisateurs\emerylo\Documents'
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 WINDOW_TITLE = "Arcade Tutoriel"
 
 def main():
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE)
-    arcade.set_background_color(arcade.color.BLUE)
+    arcade.set_background_color(arcade.color.ASH_GREY)
     arcade.start_render()
-    arcade.draw_lrtb_rectangle_filled(0, SCREEN_WIDTH, SCREEN_HEIGHT / 3, 0, arcade.csscolor.RED)
-    arcade.draw_rectangle_filled(320, 400, SCREEN_WIDTH, SCREEN_HEIGHT / 3, arcade.csscolor.WHITE)
+    points = [(0, 0), (280, 200), (280,380), (0, 480)]
+    arcade.draw_polygon_filled(points, arcade.color.GRAY)
+    points = [(640, 0), (360, 200), (360, 380), (640, 480)]
+    arcade.draw_polygon_filled(points, arcade.color.GRAY)
+    points = [(280, 200), (360, 200), (360, 380), (280,380)]
+    arcade.draw_polygon_filled(points, arcade.color.DARK_BROWN)
+    arcade.draw_circle_filled(340, 270, 10, arcade.color.GRAY)
+    sprite = arcade.Sprite("Documents/MONSTER_1.png", 1)
+
     arcade.finish_render()
     arcade.run()
 
--- main.py
+++ main.py
@@ -4,16 +4,22 @@ niveau_de_vie = 20
 numero_adversaire = 0
 regles_du_jeu = ("Vous entrez un dongeon, vous avez 20 vies. \nvous arrivez face a face avec de monstres avec un niveau de pouvoir aleatoire entre 1 et 5. \nsi vous voulez les batre, appuiez 1, si vous gagnez, vous gagnez un nombre de vies egales au nombre de pouvoir au monstre.\nsi vous perdez, vous perdez le nombre de vie egale au pouvoir du monstre. votre attaque est aleatoire entre 1 et 6. \nsi vous voulez le contourner, apuiez 2.vous allez perdre une vie et aller a un prochain monstre. \nsi vous voulez quitter la partie, apuiez 4.")
 pdv = 0
-
-
+force_adversaire = 0
+choix = 0
+def MONSTRE():
+    global force_adversaire
+    force_adversaire = random.randint(1, 5)
 def choice():
     global death
     global niveau_de_vie
     global numero_adversaire
     global regles_du_jeu
     global pdv
+    global choix
     choix = int(input("Que voulez vous faire?\n1: Attacker le monstre\n2: Controuner cet adversaire(-1 vie)\n3: Afficher les regles du jeu\n4: Quitter la partie"))
-    if choix == 1:
+    if choix == 3:
+        print(regles_du_jeu)
+    elif choix == 1:
         dice_roll = random.randint(1, 6)
         print("vous avez roule", dice_roll)
         if dice_roll <= force_adversaire:
@@ -35,17 +41,17 @@ def choice():
         if niveau_de_vie <= 0:
             print("vous etes mort apres", pdv, "monstres")
             death = True
-    elif choix == 3:
-        print(regles_du_jeu)
     elif choix == 4:
         death = True
 
     else:
         print("choix invalide.")
+    return choix
 
 while not death:
 
-    force_adversaire = random.randint(1, 5)
+    if choix != 3:
+        MONSTRE()
     print("Vous tombez face à face avec un adversaire de difficulté :", force_adversaire)
     numero_adversaire += 1
     choice()
